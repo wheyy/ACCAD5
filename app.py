dea4c5f2e534250105ac561526cabd6bf0a88155
King Whey <lowkingwhey@gmail.com>
2025-01-14 14:42:38 +0800

    Hello World!!!!!

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-    return "Hello World"
+    return "Hello World!!!!!!!"
 
 if __name__ == "__main__":
     app.run(port=8000, host='0.0.0.0')
